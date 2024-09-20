c2572ecb7cec6e7c17de780dac343bd5382386b8
Rahmanhusain <rahmanhusain899@gmail.com>
2024-09-20 13:08:33 +0530

    register and login API

---
+++ src/models/usermodel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  isteacher: {
+    type: Boolean,
+    default: false,
+  },
+  class_stu: {
+    type: String,
+    required: true
+  },
+  disability: {
+    type: String,
+    default: "None",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Check if the model already exists to avoid overwriting it during hot reloads
+export default mongoose.models.User || mongoose.model("User", UserSchema);
